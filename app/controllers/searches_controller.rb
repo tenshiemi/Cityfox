4cd4e92053e0c6072ce6fd3403e2b988d1e41606
Emily Chen <tenshiemi@gmail.com>
2014-10-25 18:00:15 +0200

    display reviews on profile page

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,6 @@
 class SearchesController < ApplicationController
+	skip_before_filter :require_login, only: [:index]
+
 	def index
 		@search_term = params[:search_term]
 		@results = Company.where("name like ?", "%#{@search_term}%")
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,4 @@
 class Review < ActiveRecord::Base
-	has_one :user
-	has_one :company
+	belongs_to :user
+	belongs_to :company
 end
