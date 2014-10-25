5ef5bc2e18b2b51f062ccdc5ac79a0479f2b76e5
Emily Chen <tenshiemi@gmail.com>
2014-10-25 23:09:55 +0200

    New owner routing working

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,2 +1,28 @@
 class OwnersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  skip_before_filter :require_login, only: [:new, :create]
+
+	def new
+		@company = Company.find(params[:id])
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(owner_params)
+    respond_to do |format|
+      if @user.save
+        auto_login(@user)
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+ 	end
+
+	private
+	def owner_params
+    params.require(:user).permit(:role, :company_id, :name, :email, :phone, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 
   post 'search' => 'searches#index'
 
+  get 'owners/new_owner' => 'owners#new', :as => :new_owner
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
