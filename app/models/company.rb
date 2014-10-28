fed4c3056cdc39773fc125c75f2412399a16dd56
Emily Chen <tenshiemi@gmail.com>
2014-10-28 10:41:53 +0100

    Added % approval and fixed login bug #5

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,4 +5,10 @@ class Company < ActiveRecord::Base
 	validates :name, presence: true
 	validates :country, presence: true
 	validates :city, presence: true
+
+	def rating
+		rating_count = self.reviews.count
+		positive_ratings = self.reviews.where(would_work_with: true).count
+		(positive_ratings.to_f / rating_count * 100).to_i
+	end
 end
