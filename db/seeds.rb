bb87660af8da4f30c1b80af9604924858b48093f
Emily Chen <tenshiemi@gmail.com>
2014-10-29 11:03:47 +0100

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: "Emily", email: "admin@test.com", password: "test", password: "test", role: "admin")
+User.create(name: "Sarah", email: "a@test.com", password: "test", password: "test")
+User.create(name: "Emi", email: "b@test.com", password: "test", password: "test")
+User.create(name: "Emmy", email: "c@test.com", password: "test", password: "test")
+
+Company.create(name: "Amazon", industry: "Software", city: "Seattle", country: "USA")
+Company.create(name: "Paperless Post", industry: "Software", city: "New York", country: "USA")
+Company.create(name: "Xing", industry: "Software", city: "Barcelona", country: "Spain")
