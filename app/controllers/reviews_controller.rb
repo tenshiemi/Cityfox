844c3ac3a5fd0cff381082922f20a04f8dce33d0
Emily Chen <tenshiemi@gmail.com>
2014-10-29 21:44:43 +0100

    displays form, still working on ajax

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -21,8 +21,18 @@ class ReviewsController < ApplicationController
     @review = Review.find(params[:id])
   end
 
+  def add_response
+    @review = Review.find(params[:id])
+    respond_to do |format|
+      if !@review.update_attributes(params[:response])
+        @error='Could not submit response.'
+      end
+      format.js
+    end
+  end
+
   private
 	def review_params
-    params.require(:review).permit(:user_id, :company_id, :title, :description, :rating_payment, :rating_communication, :rating_expectations, :would_work_with)
+    params.require(:review).permit(:user_id, :company_id, :title, :description, :rating_payment, :rating_communication, :rating_expectations, :would_work_with, :id, :response)
   end
 end
