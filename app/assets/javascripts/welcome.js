722b7d288575878ecb14b5790173744084159117
Emily Chen <tenshiemi@gmail.com>
2014-10-30 17:02:00 +0100

    slow progress

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,15 +2,18 @@ $(function() {
 	$('#search_term').focus();
 });
 
-$(function() {
-	var company_list
-	$.ajax({
+var company_list;
+$( document ).ready(function() {
+  $.ajax({
 	  type: "GET",
 	  url: "http://localhost:3000/companies/get_all",
 	}).done(function(json) {
 		company_list = json;
 	});
+});
+
+$(function() {
 	$('#search_term').keyup(function() {
-		
+		console.log($(this).value)
 	});
 });
