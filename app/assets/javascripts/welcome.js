c5367e4061b896f4b0fa9b92bd52f8a8bf5be7a4
Emily Chen <tenshiemi@gmail.com>
2014-10-30 18:40:38 +0100

    dropdown styling

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -12,9 +12,14 @@ $(function() {
 		names = _.pluck(company_list, 'name');
 	});
 	$('#search_term').keyup(function() {
+		$('#search_auto').empty();
 		current_search = $(this).val().toLowerCase();
 		var filtered = _.filter(names, function(name) {
 			return name.toLowerCase().search(current_search) != -1;
 		});
+		var maxResults = filtered.length > 5 ? 5 : filtered.length;
+		for (var i = 0; i < maxResults; i++) {
+			$('#search_auto').append("<li style=\"height: 1.4em; padding: .2em 0 .2em 0; margin-bottom: .4em;\">" + filtered[i] + "</li>");
+		}
 	});
 });
