8b6a77c5fbce26dc7350c6f1178da98865ed24fd
Emily Chen <tenshiemi@gmail.com>
2014-10-30 18:55:24 +0100

    more search styles

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -9,17 +9,19 @@ $(function() {
 	  url: "http://localhost:3000/companies/get_all",
 	}).done(function(json) {
 		company_list = json;
-		names = _.pluck(company_list, 'name');
 	});
 	$('#search_term').keyup(function() {
 		$('#search_auto').empty();
 		current_search = $(this).val().toLowerCase();
-		var filtered = _.filter(names, function(name) {
-			return name.toLowerCase().search(current_search) != -1;
+		var filtered = _.filter(company_list, function(company) {
+			return company.name.toLowerCase().search(current_search) != -1;
 		});
 		var maxResults = filtered.length > 5 ? 5 : filtered.length;
 		for (var i = 0; i < maxResults; i++) {
-			$('#search_auto').append("<li style=\"height: 1.4em; padding: .2em 0 .2em 0; margin-bottom: .4em;\">" + filtered[i] + "</li>");
+			$('#search_auto').append("<li style=\"height: 1.4em; padding: .2em 0 .2em 0; margin-bottom: .4em;\"><a href=\"/companies/"+ filtered[i].id + "\">" + filtered[i].name + "</a></li>");
 		}
+	});
+	$('body').click(function() {
+		$('#search_auto').empty();
 	});
 });
