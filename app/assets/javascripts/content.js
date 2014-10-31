2bcdcc45769399630595b01b450a489d7301c543
Emily Chen <tenshiemi@gmail.com>
2014-10-31 15:53:35 +0100

    Added change password

--- app/assets/javascripts/content.js
+++ app/assets/javascripts/content.js
@@ -1,18 +1,41 @@
-$(function() {
-  $('.trigger--form').click(function() {
-		$('.form--response').css('display', 'block');
-	});
-	$('#edit_review_1').on('submit', function(evt){
-		evt.preventDefault();
-		var valuesToSubmit = $(this).serialize();
-		$.ajax({
-		  type: "POST",
-		  url: $(this).attr('action'),
-		  data: valuesToSubmit,
-		  dataType: 'JSON'
-		}).done(function(json) {
-			$('.form--response').css('display', 'none');
-			$('p.response').text(json.res);
+var contentJS = function() {
+	$(function() {
+	  $('.trigger--form').click(function() {
+			$('.form--response').css('display', 'block');
+		});
+		$('#edit_review_1').on('submit', function(evt){
+			evt.preventDefault();
+			var valuesToSubmit = $(this).serialize();
+			$.ajax({
+			  type: "POST",
+			  url: $(this).attr('action'),
+			  data: valuesToSubmit,
+			  dataType: 'JSON'
+			}).done(function(json) {
+				$('.form--response').css('display', 'none');
+				$('p.response').text(json.res);
+			});
 		});
 	});
-});
+
+	$(function() {
+		$('.trigger--password').click(function() {
+			$('.form--password-change').css('display', 'block');
+		});
+		$('.form--password-change > form').on('submit', function(evt){
+			evt.preventDefault();
+			var valuesToSubmit = $(this).serialize();
+			$.ajax({
+			  type: "POST",
+			  url: $(this).attr('action'),
+			  data: valuesToSubmit,
+			  dataType: 'JSON'
+			}).done(function(json) {
+				$('.form--password-change').css('display', 'none');
+			});
+		});
+	});
+}
+
+$(document).ready(contentJS);
+$(document).on('page:load', contentJS);
