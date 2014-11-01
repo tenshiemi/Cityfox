4b28f98e75e1f7aad3427408ccef0cd5caa9e4b6
Emily Chen <tenshiemi@gmail.com>
2014-11-01 14:19:51 +0100

    more styles

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,8 @@ class Review < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :company
 
-	validates :title, presence: true
-	validates :description, presence: true
+	validates :title, presence: true, length: { in: 2..120 }
+	validates :description, presence: true, length: { maximum: 500 }
 	validates :would_work_with, presence: true
 	validates :user_id, presence: true
 	validates :company_id, presence: true
