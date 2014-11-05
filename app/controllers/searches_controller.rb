1ac80b5b68b488b40a81b2d313caf8befcdddd44
Emily Chen <tenshiemi@gmail.com>
2014-11-05 22:59:37 +0100

    more styling

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,5 +4,6 @@ class SearchesController < ApplicationController
 	def index
 		@search_term = params[:search_term]
 		@results = Company.where("name ilike ?", "%#{@search_term}%")
+		@results = @results.sort { |a, b| b.reviews.count <=> a.reviews.count }
 	end
 end
