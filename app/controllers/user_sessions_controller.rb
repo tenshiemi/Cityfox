63ce09e69720018c7a51f52219ef3b1e53c77608
Emily Chen <tenshiemi@gmail.com>
2014-11-06 11:17:20 +0100

    fixed redirect hopefully

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < ApplicationController
 
   def create
   	if @user = login(params[:email], params[:password])
-  		redirect_to(root_path, notice: 'Login successful')
+  		redirect_back_or_to(root_path, notice: 'Login successful')
   	else
   		flash.now[:alert] = 'Login failed'
   		render 'new'
