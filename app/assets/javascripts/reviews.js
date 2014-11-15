79b9739fe88130dcef51c224ec4cbb5e331990fd
Emily Chen <tenshiemi@gmail.com>
2014-11-15 16:01:27 -0500

    DRYing the code

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -2,36 +2,34 @@ var ratingCleanup = function(json) {
   var id = json.res;
   $('.helpful-rating.' + id).css('display', 'none');
   $('.post-helpful-rating.' + id).css('display', 'inline-block');
-}
+};
 
 var flagCleanup = function(json) {
   var id = json.res;
   $('.icon-flag.' + id).css('display', 'none');
   $('.post-flag.' + id).css('display', 'inline-block');
-}
+};
+
+var feedbackAJAX = function(elem) {
+  return $.ajax({
+    type: "POST",
+    url: elem.href
+  });
+};
 
 var reviewJS = function() {
   $('.flag').on("click",function() {
-    $.ajax({
-      type: "POST",
-      url: $(this).context.href
-    }).done(flagCleanup);
+    feedbackAJAX(this).done(flagCleanup);
     return false;
   });
 
   $('.helpful').click(function() {
-    $.ajax({
-      type: "POST",
-      url: $(this).context.href
-    }).done(ratingCleanup);
+    feedbackAJAX(this).done(ratingCleanup);
     return false;
   });
 
   $('.unhelpful').click(function() {
-    $.ajax({
-      type: "POST",
-      url: $(this).context.href
-    }).done(ratingCleanup);
+    feedbackAJAX(this).done(ratingCleanup);
     return false;
   });
 }
--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,33 +2,33 @@
 // http://stackoverflow.com/questions/17810746/use-up-and-down-arrows-for-autocomplete-search
 
 var pageJS = function() {
-	$('#search_term').focus();
+  $('#search_term').focus();
 
-	var company_list;
+  var company_list;
 
-	// Gets a list of all the companies in the DB and assigns them to a variable
-	$.ajax({
-	  type: "GET",
-	  url: "/get_all",
-	}).done(function(json) {
-		company_list = json;
-	});
+  // Gets a list of all the companies in the DB and assigns them to a variable
+  $.ajax({
+    type: "GET",
+    url: "/get_all",
+  }).done(function(json) {
+    company_list = json;
+  });
 
-	$('#search_term').keyup(function(evt) {
-		$('#search_auto').empty();
-		current_search = $(this).val().toLowerCase();
-		var filtered = company_list.filter(function(company) {
-			return company.name.toLowerCase().search(current_search) != -1;
-		});
-		var maxResults = filtered.length > 5 ? 5 : filtered.length;
-		for (var i = 0; i < maxResults; i++) {
-			$('#search_auto').append("<li style=\"height: 1.4em; padding: .2em 0 .2em 0; margin-bottom: .4em;\"><a href=\"/companies/"+ filtered[i].id + "\">" + filtered[i].name + "</a></li>");
-		}
-	});
+  $('#search_term').keyup(function(evt) {
+    $('#search_auto').empty();
+    current_search = $(this).val().toLowerCase();
+    var filtered = company_list.filter(function(company) {
+      return company.name.toLowerCase().search(current_search) != -1;
+    });
+    var maxResults = filtered.length > 5 ? 5 : filtered.length;
+    for (var i = 0; i < maxResults; i++) {
+      $('#search_auto').append("<li style=\"height: 1.4em; padding: .2em 0 .2em 0; margin-bottom: .4em;\"><a href=\"/companies/"+ filtered[i].id + "\">" + filtered[i].name + "</a></li>");
+    }
+  });
 
-	$('body').click(function() {
-		$('#search_auto').empty();
-	});
+  $('body').click(function() {
+    $('#search_auto').empty();
+  });
 }
 
 $(document).ready(pageJS);
