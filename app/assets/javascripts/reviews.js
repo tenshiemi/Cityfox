75af3c80665dd2ce97e32e09d1046c401f174125
Emily Chen <tenshiemi@gmail.com>
2014-11-15 15:44:08 -0500

    removed turbolinks and fixed spacing

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,45 +1,39 @@
 var ratingCleanup = function(json) {
-	var id = json.res;
-	$('.helpful-rating.' + id).css('display', 'none');
-	$('.post-helpful-rating.' + id).css('display', 'inline-block');
+  var id = json.res;
+  $('.helpful-rating.' + id).css('display', 'none');
+  $('.post-helpful-rating.' + id).css('display', 'inline-block');
 }
 
 var flagCleanup = function(json) {
-	var id = json.res;
-	$('.icon-flag.' + id).css('display', 'none');
-	$('.post-flag.' + id).css('display', 'inline-block');
+  var id = json.res;
+  $('.icon-flag.' + id).css('display', 'none');
+  $('.post-flag.' + id).css('display', 'inline-block');
 }
 
 var reviewJS = function() {
   $('.flag').on("click",function() {
-  	var valuesToSubmit = $(this)
-		$.ajax({
-		  type: "POST",
-		  url: $(this).context.href,
-		  data: valuesToSubmit,
-		  dataType: 'JSON'
-		}).done(flagCleanup);
-	});
+    $.ajax({
+      type: "POST",
+      url: $(this).context.href
+    }).done(flagCleanup);
+    return false;
+  });
 
   $('.helpful').click(function() {
-  	var valuesToSubmit = $(this);
-		$.ajax({
-		  type: "POST",
-		  url: $(this).context.href,
-		  data: valuesToSubmit,
-		  dataType: 'JSON'
-		}).done(ratingCleanup);
-	});
+    $.ajax({
+      type: "POST",
+      url: $(this).context.href
+    }).done(ratingCleanup);
+    return false;
+  });
 
   $('.unhelpful').click(function() {
-  	var valuesToSubmit = $(this);
-		$.ajax({
-		  type: "POST",
-		  url: $(this).context.href,
-		  data: valuesToSubmit,
-		  dataType: 'JSON'
-		}).done(ratingCleanup);
-	});
+    $.ajax({
+      type: "POST",
+      url: $(this).context.href
+    }).done(ratingCleanup);
+    return false;
+  });
 }
 
 $(reviewJS);
